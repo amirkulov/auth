d2f6cbe72a3834ba7e20d76bb4816ff3b0a1419b
garuno <dev.garuno@mail.ru>
2016-12-15 10:34:43 +0300

    first commit

---
+++ config/auth.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1852069155011589', // your App ID
+        'clientSecret'  : '49df5b47fcd0b0d3244da70eeb59333b', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'EQQc6AFbpdUXKM35mhQhEdD45',
+        'consumerSecret'    : 'jsqwvyf4iZlSNDHBfzZhRMrzgkSKzAI1UdZK4fCYKocGWDfyhv',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '828858475167-ultnr5vgm9bu9deqg98d28eede55anae.apps.googleusercontent.com',
+        'clientSecret'  : '7IOwrAP0QbVlLhY5VJ92qpRS',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    },
+
+    'vkontakteAuth' : {
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : 'http://localhost:8080/auth/vkontakte/callback'
+    }
+
+};
